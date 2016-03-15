0f17c51f8ba4e271766bd6a138731db9bd8feee4
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-03-15 12:39:51 -0400

    Used ajax to import data. Finished styling it.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 // Javascript Entry Point
+
+import $ from 'jquery';
+
+var url = 'https://api.github.com/users/jisaacks/orgs?access_token=ac49358f7dcbd22a5887ff8fda3dedbeeed5198d';
+
+var $main = $('main');
+
+$.ajax(url).then(function(array){
+
+	$main.append(array.map(function({login, avatar_url}){
+		return `<div><img src="${avatar_url}"><h2>${login}</h2></div>`
+	}));
+	
+});
